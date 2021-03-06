a17b0c92b778a5ec49e316042ac447690e2b5bac
mlwache <mlwache@users.noreply.github.com>
2021-03-06 01:44:16 +0100

    added setup.py for testing pip install in colab

---
+++ setup.py
@@ -0,0 +1,13 @@
+ 
+from distutils.core import setup
+
+import sys
+
+setup(
+    name="pessimistic-agents",
+    version="1.0",
+    description="TODO",
+    author="TODO",
+    url="none",
+    packages=["pessimistic-agents"],
+)
