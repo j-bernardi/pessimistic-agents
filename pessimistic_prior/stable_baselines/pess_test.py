1578d231e9b3652c6b923743aac2bb869cead930
jamie <bernardi.james.h@gmail.com>
2021-03-13 21:36:09 +0000

    Adding PDQN test script
    Plus updating readme for checking out PDQN code

---
+++ pessimistic_prior/stable_baselines/pess_test.py
@@ -0,0 +1,42 @@
+import gym
+
+from collections import Counter
+
+from stable_baselines3 import PDQN
+
+env = gym.make('CartPole-v1')
+
+quick_args = {
+	# buffer_size: int = 1000000,
+    # "learning_starts": 500,
+    # "target_update_interval": 100,
+    # "exploration_fraction": 0.1,
+    # "exploration_initial_eps": 0.5,
+    # "exploration_final_eps": 0.01,
+}
+
+
+model = PDQN('MlpPessPolicy', env, verbose=1, **quick_args)
+
+
+model.learn(total_timesteps=100000)
+
+print("Queries after train")
+print(len(model.queries), Counter(model.queries))
+
+queries = []
+
+obs = env.reset()
+for i in range(1000):
+    action, _states, queried = model.pess_predict(obs, deterministic=True)
+    if queried:
+    	queries.append(i)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+      obs = env.reset()
+
+env.close()
+
+print("Queries after test")
+print(len(queries), Counter(queries))
