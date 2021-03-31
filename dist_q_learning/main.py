2d59159133e20d2b5cfb1cc3af4576bf6219b19b
jamie <bernardi.james.h@gmail.com>
2021-03-31 03:24:23 +0100

    Add random_safe mentor, better rendering, prudent mentor centre
    A few changes it made sense to do together:
    
    1. Adding a random_safe mentor which takes a random step as long
       as it doens't push the agent into a cliff region.
    
       This meant changing mentors so that they return action
       tuples rather than ints, and then converting the tuples to
       ints in the agent (more dependence on 2d case).
    
    2. Rendering such that the map is printed, and then overwritten
       by the next state so the terminal window stays fixed in place
    
    3. Fixing a small bug in the prudent mentor where the central
       state only ever broke ties "randomly" by moving up or down,
       whereas left and right should have been valid too.

--- dist_q_learning/main.py
+++ dist_q_learning/main.py
@@ -2,27 +2,37 @@ import argparse
 
 from env import FiniteStateCliffworld
 from agents import FinitePessimisticAgent
-from mentors import random_mentor, prudent_mentor
+from mentors import random_mentor, prudent_mentor, random_safe_mentor
+from transition_defs import (
+    deterministic_uniform_transitions, edge_cliff_reward_slope)
 
-MENTORS = {"prudent": prudent_mentor, "random": random_mentor}
+MENTORS = {
+    "prudent": prudent_mentor,
+    "random": random_mentor,
+    "random_safe": random_safe_mentor
+}
+TRANSITIONS = {
+    "0": deterministic_uniform_transitions,
+    "1": edge_cliff_reward_slope
+}
 
 
 def env_visualisation(env):
     print("RESET STATE")
     env.reset()
-    env.render()
+    env.render(in_loop=False)
 
     print("\n\nStep every action")
     for action in range(0, 4):
         print("TAKE ACTION", action)
         returned = env.step(action)
         print("Return tuple: ", returned)
-        env.render()
+        env.render(in_loop=False)
     print("\n\nStep off the edge")
     rew, done = None, False
     while not done:
         obs, rew, done, _ = env.step(0)
-        env.render()
+        env.render(in_loop=False)
         print("Reward, done:", rew, done)
     assert rew == -0.
 
@@ -40,6 +50,11 @@ def get_args():
         "--mentor", "-m", default="prudent", choices=list(MENTORS.keys()),
         help="The mentor providing queried actions."
     )
+    trans_help = "\n".join(
+        [f"{k}: {v.__name__}" for k, v in TRANSITIONS.items()])
+    parser.add_argument(
+        "--trans", "-t", default="0", choices=list(TRANSITIONS.keys()),
+        help=f"The mentor providing queried actions.\n{trans_help}")
 
     parser.add_argument("--num-episodes", "-n", default=0, type=int)
     parser.add_argument("--render", "-r", action="store_true", default=False)
