636bef2716d5729c94b52518bcf3696908aa186f
jamie <bernardi.james.h@gmail.com>
2021-04-29 21:19:56 +0100

    Setting up experiment dir for new experiments
    Some functions will be the same - splitting these out.

--- dist_q_learning/experiments/teleporter/exp_main.py
+++ dist_q_learning/experiments/teleporter/exp_main.py
@@ -5,20 +5,19 @@ from main import run_main
 from agents import QUANTILES
 
 from experiments.utils import save_dict_to_pickle, experiment_main
-from experiments.core_experiment import EXPERIMENT_PATH
-from experiments.core_experiment.plotter import (
-    plot_experiment_separate)  # , plot_experiment_together)
+from experiments.teleporter import EXPERIMENT_PATH
+from experiments.teleporter.plotter import plot_experiment
 
 
-def run_core_experiment(
-        results_file, agent, trans, report_every_n, mentor, steps=500,
-        earlystop=0, init_zero=False, repeat_n=0, render=-1
+def run_teleport_experiment(
+        results_file, agent, trans, n, mentor, steps=500, earlystop=0,
+        init_zero=False, repeat_n=0, render=-1
 ):
+    raise NotImplementedError("Not written for teleporting yet")
     repeat_str = f"_repeat_{repeat_n}"
     args = [
-        "--trans", trans, "--report-every-n", str(report_every_n),
-        "--mentor", mentor, "--n-steps", str(steps),
-        "--early-stopping", str(earlystop),
+        "--trans", trans, "--num-episodes", str(n), "--mentor", mentor,
+        "--steps-per-ep", str(steps), "--early-stopping", str(earlystop),
         "--render", str(render)
     ]
 
@@ -28,7 +27,7 @@ def run_core_experiment(
     # pessimistic only
     for quant_i in [q for q in quantiles if QUANTILES[q] <= 0.5]:
         q_i_pess_args = pess_agent_args + ["--quantile", str(quant_i)]
-        q_i_pess_args += ["--init", "zero"] if init_zero else []
+        q_i_pess_args += ["--init-zero"] if init_zero else []
         trained_agent = run_main(q_i_pess_args)
 
         exp_name = f"quant_{quant_i}" + repeat_str
@@ -36,12 +35,13 @@ def run_core_experiment(
         result_i = {
             exp_name: {
                 "quantile_val": QUANTILES[quant_i],
-                "queries": trained_agent.mentor_queries_periodic,
-                "rewards": trained_agent.rewards_periodic,
-                "failures": trained_agent.failures_periodic,
+                "steps_per_ep": steps,
+                "queries": trained_agent.mentor_queries_per_ep,
+                "rewards": trained_agent.rewards_per_ep,
+                "failures": trained_agent.failures_per_ep,
                 "metadata": {
                     "args": args,
-                    "steps_per_report": report_every_n,
+                    "steps_per_ep": steps,
                     "min_nonzero": trained_agent.env.min_nonzero_reward,
                     "max_r": trained_agent.env.max_r,
                 }
@@ -58,12 +58,12 @@ def run_core_experiment(
     mentor_result = {
         mentor_exp_name: {
             "quantile_val": -1.,
-            "queries": mentor_agent_info.mentor_queries_periodic,
-            "rewards": mentor_agent_info.rewards_periodic,
-            "failures": mentor_agent_info.failures_periodic,
+            "queries": mentor_agent_info.mentor_queries_per_ep,
+            "rewards": mentor_agent_info.rewards_per_ep,
+            "failures": mentor_agent_info.failures_per_ep,
             "metadata": {
                 "args": args,
-                "steps_per_report": report_every_n,
+                "steps_per_ep": steps,
                 "min_nonzero": mentor_agent_info.env.min_nonzero_reward,
                 "max_r": mentor_agent_info.env.max_r,
             }
@@ -74,19 +74,17 @@ def run_core_experiment(
 
 if __name__ == "__main__":
     results_dir = os.path.join(EXPERIMENT_PATH, "results")
-    os.makedirs(results_dir, exist_ok=True)
-
     N_REPEATS = 7
     exp_config = {
         "agent": "pess",
         "trans": "2",  # non-stochastic, sloped reward
-        "report_every_n": 100,
-        "steps": 500,
+        "n": 100,
+        "steps": 200,
         "mentor": "random_safe",  # for exploration
         "earlystop": 0,  # hard to know the right place to stop - just do it
         "init_zero": True,  # This helps remove failures
     }
 
     experiment_main(
-        results_dir, N_REPEATS, run_core_experiment, exp_config,
-        plot_experiment_separate)
+        results_dir, N_REPEATS, run_teleport_experiment, exp_config,
+        plot_experiment)
---
+++ dist_q_learning/experiments/utils.py
@@ -0,0 +1,63 @@
+import os
+import pickle
+
+
+def experiment_main(
+        results_dir, n_repeats, experiment_func, exp_config, plotting_func):
+    """Handles result file creation and repeat runs of an experiment
+
+    """
+    os.makedirs(results_dir, exist_ok=True)
+
+    f_name_no_ext = os.path.join(
+        results_dir, "_".join([f"{k}_{str(v)}" for k, v in exp_config.items()]))
+    dict_loc = f_name_no_ext + ".p"
+
+    if os.path.exists(dict_loc):
+        run = input(f"Found {dict_loc}\nOverwrite? y / n / a\n")
+    else:
+        print("No file", dict_loc, "\nrunning")
+        run = "y"
+
+    if run in ("y", "a"):
+        if run == "y" and os.path.exists(dict_loc):
+            os.remove(dict_loc)
+
+        for i in range(n_repeats):
+            print("\n\nREPEAT", i, "/", n_repeats)
+            experiment_func(dict_loc, repeat_n=i, **exp_config)
+
+    with open(dict_loc, "rb") as f:
+        results_dict = pickle.load(f)
+
+    plotting_func(results_dict, f_name_no_ext + ".png")
+
+
+def save_dict_to_pickle(filename, new_result):
+    """Append an item to results dictionary, cached in pickle file
+
+    Given:
+        results = {0: "a", 1: "b"}
+        new_result = {2: "c"}
+    Result:
+        results = {0: "a", 1: "b", 2: "c"}
+    """
+    # Load
+    if os.path.exists(filename):
+        with open(filename, 'rb') as f:
+            results = pickle.load(f)
+    else:
+        results = {}
+    # Append
+    for k in new_result.keys():
+        if k in results.keys():
+            yes = input(f"Key {k} already in dict - replace?\ny/ n")
+            if yes == "y":
+                del results[k]
+            else:
+                raise KeyError(f"{k} already in results {results.keys()}")
+    new_results = {**results, **new_result}
+
+    # Save
+    with open(filename, 'wb') as fl:
+        pickle.dump(new_results, fl, protocol=pickle.HIGHEST_PROTOCOL)
