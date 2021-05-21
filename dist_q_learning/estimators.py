f2f3e2062a21402dbac3e2a729ba892b61e7db4e
jamie <bernardi.james.h@gmail.com>
2021-05-22 00:28:11 +0100

    Adding beta plotting
    Plots beta with a fake sampling distribution

--- dist_q_learning/estimators.py
+++ dist_q_learning/estimators.py
@@ -1,41 +1,10 @@
 import abc
 import copy
 import numpy as np
-import matplotlib.pyplot as plt
 import scipy.stats
-from scipy.interpolate import UnivariateSpline
 
 # import pygln
 import glns
-
-NP_RANDOM_GEN = np.random.Generator(np.random.PCG64())
-
-
-def sample_beta(a, b, n=1):
-    """Sample Beta(alpha=a, beta=b), return 1d array size n."""
-    return NP_RANDOM_GEN.beta(a, b, size=n)
-
-
-def plot_beta(a, b, show=True, n_samples=10000):
-    """Plot a beta distribution, given these parameters."""
-    ax = plt.gca()
-    sampled_vals = sample_beta(a, b, n=n_samples)
-    n_bins = n_samples // 100
-    ps, xs = np.histogram(sampled_vals, bins=n_bins, density=True)
-    # convert bin edges to centers
-    xs = xs[:-1] + (xs[1] - xs[0]) / 2
-    f = UnivariateSpline(xs, ps, s=n_bins)  # smooths data
-
-    ax.set_title(f"Beta distribution for alpha={a}, beta={b}")
-    ax.set_ylabel("PDF")
-    ax.set_xlabel("E(reward)")
-    ax.set_xlim((0, 1))
-
-    ax.plot(xs, f(xs))
-    if show:
-        plt.show()
-
-    return ax
 
 
 class Estimator(abc.ABC):
--- dist_q_learning/utils.py
+++ dist_q_learning/utils.py
@@ -1,6 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.interpolate import UnivariateSpline
+
+NP_RANDOM_GEN = np.random.Generator(np.random.PCG64())
+
+
 def geometric_sum(r_val, gamm, steps):
     # Two valid ways to specify infinite steps
     if steps is None or steps == "inf":
         return r_val / (1. - gamm)
     else:
         return r_val * (1. - gamm ** steps) / (1. - gamm)
+
+
+def sample_beta(a, b, n=1):
+    """Sample Beta(alpha=a, beta=b), return 1d array size n."""
+    return NP_RANDOM_GEN.beta(a, b, size=n)
+
+
+def get_beta_plot(alpha, beta, n_samples):
+    """Returns xs and f (as in f(xs)) needed to plot the beta curve"""
+    sampled_vals = sample_beta(alpha, beta, n=n_samples)
+    n_bins = n_samples // 100
+    ps, xs = np.histogram(sampled_vals, bins=n_bins, density=True)
+    # convert bin edges to centers
+    xs = xs[:-1] + (xs[1] - xs[0]) / 2
+    f = UnivariateSpline(xs, ps, s=n_bins)  # smooths data
+    return xs, f
+
+
+def plot_beta(a, b, show=True, n_samples=10000):
+    """Plot a beta distribution, given these parameters."""
+    ax = plt.gca()
+    xs, f = get_beta_plot(a, b, n_samples)
+
+    ax.set_title(f"Beta distribution for alpha={a}, beta={b}")
+    ax.set_ylabel("PDF")
+    ax.set_xlabel("E(reward)")
+    ax.set_xlim((0, 1))
+
+    ax.plot(xs, f(xs))
+    if show:
+        plt.show()
+
+    return ax
