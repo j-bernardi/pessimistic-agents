2860463a2563e35acc96dae9b597893f88fd7ba8
jamie <bernardi.james.h@gmail.com>
2021-05-23 00:10:02 +0100

    Adding plot for risk-avoidance experiment
    To be shown in presentation. Tuned for the one being shown, so some
    text positions may need fiddling in the future. Sorry, future-me.

--- dist_q_learning/experiments/event_experiment/joint_experiment.py
+++ dist_q_learning/experiments/event_experiment/joint_experiment.py
@@ -5,16 +5,21 @@ from experiments.exp_utils import experiment_main
 from experiments import EXPERIMENTS_DIR
 
 from experiments.event_experiment.exp_main import run_event_avoid_experiment
-from experiments.event_experiment.plotter import compare_transitions
+from experiments.event_experiment.plotter import (
+    compare_transitions, nice_compare_transitions)
 from experiments.core_experiment.finite_agent_0 import run_core_experiment
 from experiments.core_experiment.plotter import plot_experiment_separate
 
 from experiments.event_experiment.configs.every_state import all_configs
 
 if __name__ == "__main__":
-    RESULTS_DIR = os.path.join(EXPERIMENTS_DIR, "final_exp_1")
+    RESULTS_DIR = os.path.join(EXPERIMENTS_DIR, "results_final_present")
     os.makedirs(RESULTS_DIR, exist_ok=True)
     N_REPEATS = 15
+
+    options = (
+        (compare_transitions, "_events"),
+        (nice_compare_transitions, "_nice_events"))
 
     for cfg in all_configs:
         experiment_main(
@@ -22,11 +27,12 @@ if __name__ == "__main__":
             n_repeats=N_REPEATS,
             experiment_func=run_event_avoid_experiment,
             exp_config=cfg,
-            plotting_func=compare_transitions,
+            plotting_func=options[1][0],
             show=False,
-            plot_save_ext="_events"  # essential - so as not to overwrite image
+            plot_save_ext=options[1][1],  # essential - not to overwrite image
+            save=True,
+            overwrite=False,
         )
-
         experiment_main(
             results_dir=RESULTS_DIR,
             n_repeats=N_REPEATS,
