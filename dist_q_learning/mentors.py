c688acda714f7b091f2c1ead53bc10112525c1df
jamie <bernardi.james.h@gmail.com>
2021-07-25 18:01:55 +0100

    Make mentor target_v more random
    Target -0.1 or 0.1 velocity, 1:7 ratio with random choice

--- dist_q_learning/mentors.py
+++ dist_q_learning/mentors.py
@@ -208,8 +208,10 @@ def cartpole_safe_mentor_normal(state, target_centre=True, **kwargs):
         v_target = jnp.clip(
             -(x - x_target) * min_velocity_scale, -max_velocity, max_velocity)
     else:
-        # target 0.1 velocity instead (configurable)
-        v_target = kwargs.get("v_target", 0.1)
+        # 1:7 ratio
+        # v_target = kwargs.get("v_target", 0.1)
+        v_target = random.choice(
+            [-0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1])
 
     min_theta_scale = 4.  # rate at which to bring v to v target
     max_theta = 0.2  # Max acceptable pole angle
