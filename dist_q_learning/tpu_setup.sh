e9b43d5dc57f7371e64bcf61ef22cdf2a8b659b2
jamie <bernardi.james.h@gmail.com>
2021-11-30 11:31:55 +0000

    Adding git clone to setup script

--- dist_q_learning/tpu_setup.sh
+++ dist_q_learning/tpu_setup.sh
@@ -1,3 +1,4 @@
+git clone https://github.com/j-bernardi/pessimistic-agents.git
 pip install -r conda_envs/tpu_requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 source set_path.sh
 export STORAGE_BUCKET=gs://free-tpu-bucket
