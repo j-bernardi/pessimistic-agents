7c0fac8577767e7872071734befc8c8fa7b12406
jamie <bernardi.james.h@gmail.com>
2021-12-02 13:49:45 +0000

    Fixsetup script

--- dist_q_learning/tpu_setup.sh
+++ dist_q_learning/tpu_setup.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/j-bernardi/pessimistic-agents.git
-pip install -r conda_envs/tpu_requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install -r conda_envs/tpu_requirements.txt # -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 source set_path.sh
 export STORAGE_BUCKET=gs://free-tpu-bucket
 echo "Storage dir: $STORAGE_BUCKET"
+tmux
