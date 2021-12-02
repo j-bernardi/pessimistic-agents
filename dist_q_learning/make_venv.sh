5db50e743a8de5690c5d29c5314eed20df27fcfd
jamie <bernardi.james.h@gmail.com>
2021-12-02 17:08:55 +0000

    Using virtual envs

---
+++ dist_q_learning/make_venv.sh
@@ -0,0 +1,1 @@
+source virt_env/bin/activate
--- dist_q_learning/tpu_setup.sh
+++ dist_q_learning/tpu_setup.sh
@@ -1,5 +1,6 @@
+python3 -m venv virt_env
+source virt_env/bin/activate
 pip install -r conda_envs/tpu_requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 source set_path.sh
 export STORAGE_BUCKET=gs://free-tpu-bucket
 echo "Storage dir: $STORAGE_BUCKET"
-tmux
