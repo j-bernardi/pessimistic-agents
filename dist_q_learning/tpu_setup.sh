985a83b5cfb5f9360aee618ab6c8868a0366fdf3
jamie <bernardi.james.h@gmail.com>
2021-12-02 17:02:20 +0000

    Fixing env order and paths

--- dist_q_learning/tpu_setup.sh
+++ dist_q_learning/tpu_setup.sh
@@ -1,4 +1,4 @@
-pip install -r conda_envs/tpu_requirements.txt # -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+pip install -r conda_envs/tpu_requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 source set_path.sh
 export STORAGE_BUCKET=gs://free-tpu-bucket
 echo "Storage dir: $STORAGE_BUCKET"
