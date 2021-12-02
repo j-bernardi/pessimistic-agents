78fb1ee43fdc3b3c071f3905a9b306d5ce495a37
jamie <bernardi.james.h@gmail.com>
2021-12-02 17:13:20 +0000

    Install venv

--- dist_q_learning/tpu_setup.sh
+++ dist_q_learning/tpu_setup.sh
@@ -1,5 +1,8 @@
+sudo apt-get update
+sudo apt-get install python3-venv
 python3 -m venv virt_env
 source virt_env/bin/activate
+python3 -m pip install --upgrade pip
 pip install -r conda_envs/tpu_requirements.txt -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 source set_path.sh
 export STORAGE_BUCKET=gs://free-tpu-bucket
