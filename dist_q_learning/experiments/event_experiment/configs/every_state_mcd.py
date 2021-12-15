bf77aaf3f5bafc6edcfecb0c616b33894b40c0ed
peterbarnettnz <peterbarnettnz@gmail.com>
2021-12-15 16:09:03 +0000

    adding the config

---
+++ dist_q_learning/experiments/event_experiment/configs/every_state_mcd.py
@@ -0,0 +1,43 @@
+WIDTH = 7
+
+# Fixed for all experiments
+base_exp = {
+    "agent": "continuous_pess_mcd",
+    "mentor": "cartpole_sweep",
+    "report_every_n": 32,
+    "steps": 150000,
+    "burnin_n": 5000,
+    "sampling_strat": "random",
+    "batch_size": 64,
+    "update_freq": 32,
+    "init_zero": False,  # Not implemented for glns
+}
+
+gammas = [0.9, 0.95, 0.99]
+lrs = [0.1, 0.08, 0.05]
+lr_steps = [(50, 0.97), (80, 0.98), (100, 0.99), (120, 0.98)]
+quantiles = ["0", "1", "2", "3", "4", "5"]
+dropout_rates = [0.1, 0.5, 0.9]
+hidden_sizes_options = [[1024,16], [89,128,5]]
+
+mentor_run = {
+    "quantile": "mentor",
+    "learning_rate": 1.,
+}
+
+all_configs = [{**mentor_run, **base_exp}]
+for lr in lrs:
+    for q in quantiles:
+        for lr_step in lr_steps:
+            for gamma in gammas:
+                for dropout_rate in dropout_rates:
+                    for hidden_sizes in hidden_sizes_options:
+                        config_params = {
+                            "gamma": gamma,
+                            "quantile": q,
+                            "learning_rate": lr,
+                            "learning_rate_step": lr_step,
+                            "dropout_rate": dropout_rate,
+                            "hidden_sizes": hidden_sizes,
+                        }
+                        all_configs.append({**config_params, **base_exp})
